daeaf984c084c8a47d6017f22c0145003b964756
AntonGoncharik <ant.goncharik@gmail.com>
2022-03-08 00:52:04 +0300

    Add initial settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppService } from './app.service';
+import { DatabaseModule } from '@database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
